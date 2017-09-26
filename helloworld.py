8f54e705c97dc50f7302bfd609518144ec8c367b
xiaozefeng <qq523107430@163.com>
2017-09-26 18:03:19 +0800

    added hello world list my_abc files

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+print('please enter your name')
+name=input()
+print('Hello',name)
---
+++ my_abs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+def my_abc(x):
+    if x >=0:
+        return x
+    return -x
+
