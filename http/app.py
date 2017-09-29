76f2ece9164d10a2aa62910d1344567f2950c2bd
xiaozefeng <qq523107430@163.com>
2017-09-29 17:42:02 +0800

    added flask

--- http/app.py
+++ http/app.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
+app= Flask(__name__)
 
-app = Flask(__name__)
-
-@app.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET'])
 def home():
-    return '<h1>Home</h1>'
+    return render_template('home.html')
 
 @app.route('/signin',methods=['GET'])
 def signin_form():
-    return '''<form action="/signin" method="post">
-              <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
-              <p><button type="submit">Sign In</button></p>
-              </form>
-           '''
+    return render_template('form.html')
 
 @app.route('/signin',methods=['POST'])
-def singin():
-    if request.form['username']== 'admin' and request.form['password'] == 'password':
-        return '<h3>Hello,admin</h3>'
-    return '<h3> Bad username or password.</h3>'
+def signin():
+    username= request.form['username']
+    password= request.form['password']
+    if username == 'admin' and password == 'admin':
+        return render_template('signin-ok.html',username=username)
+    return render_template('form.html',message='Bad username or password',username = username)
 
 if __name__ == '__main__':
     app.run()
