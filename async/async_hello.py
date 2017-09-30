61a21cd6223aeafc088534534b9bd0b8f6c6dba2
肖泽锋 <qq523107430@163.com>
2017-09-30 08:27:50 +0800

    added asyncio

---
+++ async/async_hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio,threading
+
+@asyncio.coroutine
+def hello():
+    print('Hello World1! (%s)' % threading.currentThread())
+    yield from asyncio.sleep(2)
+    print('Hello World2! (%s)' % threading.currentThread())
+
+loop = asyncio.get_event_loop()
+tasks = [hello(),hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
---
+++ async/coroutine.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 协程，又称微线程，纤程。英文名Coroutine。
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] consuming %s'%n)
+        r = '200 OK'
+
+def produce(c):
+    # 启动生成器
+    c.send(None)
+    n = 0
+    while n<5:
+        n += 1
+        print('[PRODUCER] Producing %s'%n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s'% r)
+    c.close()
+
+c = consumer()
+produce(c)
