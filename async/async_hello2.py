90e6c839c46c3939d3b1b3add76bf9a2662e4a11
肖泽锋 <qq523107430@163.com>
2017-10-16 17:08:15 +0800

    added async files

---
+++ async/async_hello2.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+import threading
+import asyncio
+
+async def hello():
+    print('Hello World! %s' % threading.currentThread())
+    await asyncio.sleep(1)
+    print('Hello Again! %s' % threading.currentThread())
+
+
+loop = asyncio.get_event_loop()
+tasks = [hello(),hello()]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
